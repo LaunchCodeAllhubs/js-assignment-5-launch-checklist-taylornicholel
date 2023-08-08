a4415890cd14f880bc9c1e297635a09d371aeced
taylornicholel <139183007+taylornicholel@users.noreply.github.com>
2023-08-07 20:13:01 -0500

    form submission valudation hiccups

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,32 +3,78 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
+   // why the extra indent? Do I need to extract the variables here too?
    let missionTarget = document.getElementById('missionTarget');
         missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name} </li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
                 <img src="${imageUrl}">
         `
 }
 
 function validateInput(testInput) {
+    // testing numeric data to ensure countablility 
     if (testInput === "" || testInput === null || testInput === 0) {
-        return `Empty`
+        return `Empty` // Would 0000 return an invalid submission? or 0.0?
     } else if ((!isNaN(Number(testInput)))) {
-        return `Is a Number`
+        return `Is a Number`// used to check co/pilot entries
     } else {
-        return 'Not a Number'
+        return 'Not a Number' // used to check fuel and cargo entries
     }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   // gather user input while also  viability and functionality
+   // DOM elements from index even though we didn't access the parent/sibling file (formulas are exported at the bottom)
+   let pilotStatus = document.getElementById('pilotStatus');
+   let copilotStatus = document.getElementById('copilotStatus');
+   let fuelStatus = document.getElementById('fuelStatus');
+   let launchStatus = document.getElementById('launchStatus');
+   let cargoStatus = document.getElementById('cargoStatus');
+
+
+   // all fields have an entry
+   if (validateInput(pilot) === `Empty`|| validateInput(copilot) === `Empty`|| 
+   validateInput(fuelLevel) === `Empty`||validateInput(cargoLevel) === `Empty`) {
+       alert(`All fields are required`); // Empty is a local variable defined in previous function (Don't mix up concepts with variables)
+   }
+   // fuelLevel and cargoLevel are numbers +++ pilot and co-pilot are strings
+   else if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
+       alert(`Please enter numerical values for Fuel Level and Cargo Mass`);
+   } else if (validateInput(pilot)===`Is a Number`||validateInput(copilot)===`Is a Number`) {
+       alert('Please do not enter numbers for name of pilot or co-pilot');
+   } else { 
+       // update co/pilot status
+       pilotStatus.innerHTML = `Pilot ${pilot} is ready`;
+       copilotStatus.innerHTML = `Co-pilot ${copilot} is ready`;
+       list.style.visibility = 'hidden'; // double check this with Carrie's recording about hidden visibility
+   }
+
+
+   //check fuel levels and update faulty items
+   if (Number(fuelLevel) < 10000) {
+       fuelStatus.innerHTML = `Not enough fuel for journey`;
+       list.style.visibility = 'visible';
+       launchStatus.innerHTML = `Shuttle not ready for launch`;
+       launchStatus.style.color = `red`;
+   } else if (Number(cargoLevel) > 10000) {
+       cargoStatus.innerHTML = `Cargo too heavy for takeoff`;
+       list.style.visibility = `visible`;
+       launchStatus.innerHTML = `Shuttle not ready for launch`;
+       launchStatus.style.color = `red`;
+   } else if (Number(cargoLevel) < 10000 && Number(fuelLevel) > 10000) {
+       list.style.visibility = `visible`;
+       fuelStatus.innerHTML = `Enough fuel for journey`;
+       cargoStatus.innerHTML = `Cargo light enough for takeoff`;
+       launchStatus.innerHTML = `Shuttle ready for launch`;
+       launchStatus.style.color = `green`;
+   }
 }
 
 async function myFetch() {
