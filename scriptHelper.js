c032ac12f24a37d8460f5d9e4a538f607513960d
taylornicholel <139183007+taylornicholel@users.noreply.github.com>
2023-08-10 08:51:45 -0500

    final stretch github

--- scriptHelper.js
+++ scriptHelper.js
@@ -87,7 +87,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         cargoStatus.innerHTML = `Cargo mass low enough for launch`;
         launchStatus.innerHTML = `Shuttle is Ready for Launch`;
         launchStatus.style.color = 'rgb(65, 159, 106)'; // green
-    };
+    };   
 
 
 
